67c51a8af29541afd137b3791005fc8c6e76dd08
deepak <deepamdeebu@gmail.com>
2023-12-19 16:32:36 +0530

    Lock : Used Redis facade options to lock cuncurrent job

--- app/Jobs/TestJob3.php
+++ app/Jobs/TestJob3.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 
 class TestJob3 implements ShouldQueue
 {
@@ -30,10 +31,31 @@ class TestJob3 implements ShouldQueue
     {
         // throw new \Exception("this is a sample exception");
 
-        Cache::lock('job3_lock')->block(10, function() {
+        // Redis::funnel('job3_lock')
+        // ->limit(1)
+        // ->block(10)
+        // ->then(function(){
+        //     sleep(5);
+        //     info("running job 3");
+        // });
+
+        // Below code expects exception
+        // it allow only one instance of job3 to run for 60 secconds but the other batch will only wait for 10 seconds
+
+        Redis::throttle('job3_lock')
+        ->allow(1)
+        ->every(60)
+        ->block(10)     // check for lock release for sec
+        ->then(function(){
             sleep(5);
             info("running job 3");
         });
+
+
+        // Cache::lock('job3_lock')->block(10, function() {
+        //     sleep(5);
+        //     info("running job 3");
+        // });
 
         logger("This is test job 3");
     }
