48cd861422e7332e1b45b01eb9dc74171ef67baa
deepak <deepamdeebu@gmail.com>
2023-12-31 17:17:19 +0530

    Route : Added a basic route inside package to test

--- package/todo/src/TestServiceProvider.php
+++ package/todo/src/TestServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 class TestServiceProvider extends ServiceProvider {
 
     public function boot() {
-
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
     }
 
     public function register() {
---
+++ package/todo/src/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+Route::get('/test-package', function() {
+    dd("this is a test package");
+});
+
+?>
